824262c94818ca2149d220b22eb814eab6034f0c
unknown <bhardwajdev269@gmail.com>
2025-12-07 23:38:59 +0530

    installed tooltip + added Dock footer

---
+++ src/components/Dock.jsx
@@ -0,0 +1,29 @@
+import { dockApps } from '#constants';
+import React, { useRef } from 'react'
+import { Tooltip } from 'react-tooltip';
+
+const Dock = () => {
+    const dockRef = useRef(null);
+    const toggleApp = () => {};
+  return (
+    <section id='dock'>
+        <div ref={dockRef} className='dock-container'>
+            {dockApps.map((app) => (
+                <div key={app.id} className='flex relative justify-center'>
+                    <button type='button' className='dock-icon' aria-label={app.name} data-tooltip-id="dock-tooltip" data-tooltip-content={app.name} data-tooltip-delay-show={150} disabled={!app.canOpen} onClick={() => toggleApp({id: app.id, canOpen: app.canOpen})}>
+                        <img 
+                        src={`/images/${app.icon}`}
+                        alt={app.name}
+                        loading='lazy'
+                        className={app.canOpen ? '' : 'opacity-60'}
+                        />
+                    </button>
+                </div>
+            ))}
+            <Tooltip id="dock-tooltip" place="top" className="tooltip"/>
+        </div>
+    </section>
+  )
+}
+
+export default Dock
