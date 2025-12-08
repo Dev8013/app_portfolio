39f33dd0c59bdd2af2d71b130ed88816174dba4d
unknown <bhardwajdev269@gmail.com>
2025-12-08 09:17:51 +0530

    Fixed closing of window tab (Fixed error in useLayoutEffect Hook)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import Dock from '#components/Dock'
 import Navbar from '#components/Navbar'
 import Welcome from '#components/Welcome'
 import React from 'react'
+import { gsap } from 'gsap'
+import { Draggable } from 'gsap/Draggable'
+import Terminal from '#windows/Terminal'
+gsap.registerPlugin(Draggable);
 
 const App = () => {
   return (
@@ -9,6 +13,8 @@ const App = () => {
       <Navbar />
       <Welcome />
       <Dock />
+
+      <Terminal />
     </main>
   )
 }
--- src/store/window.js
+++ src/store/window.js
@@ -7,23 +7,27 @@ const useWindowStore = create(immer((set) => ({
     windows: WINDOW_CONFIG,
     nextZIndex: INITIAL_Z_INDEX + 1,
 
+    //RECHECK
     //functions
     openWindow: (windowKey, data = null) => set((state) => {
         const win = state.windows[windowKey];
+        if(!win) return;
         win.isOpen = true;
         win.zIndex = state.nextZIndex;
-        win.data = data;
+        win.data = data ?? win.data;
         state.nextZIndex++;
     }),
     closeWindow: (windowKey) => set((state) => {
         const win = state.windows[windowKey];
+        if(!win) return;
         win.isOpen = false;
         win.zIndex = INITIAL_Z_INDEX;
         win.data = null;
     }),
     focusWindow: (windowKey) => set((state) => {
         const win = state.windows[windowKey];
-        win.zIdex = state.nextZIndex++;
+        if(!win) return;
+        win.zIndex = state.nextZIndex++;
     })
     
 })));
