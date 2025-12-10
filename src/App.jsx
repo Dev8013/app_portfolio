1647bcca68458fde15c82cc3a2127eec9161bdca
unknown <bhardwajdev269@gmail.com>
2025-12-10 16:15:44 +0530

    implemented Contact Window

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Safari from '#windows/Safari'
 import Resume from '#windows/Resume'
 import Finder from '#windows/Finder'
 import Text from '#windows/Text'
+import Contact from '#windows/Contact'
 gsap.registerPlugin(Draggable);
 
 const App = () => {
@@ -23,6 +24,7 @@ const App = () => {
       <Resume />
       <Finder />
       <Text />
+      <Contact />
     </main>
   )
 }
